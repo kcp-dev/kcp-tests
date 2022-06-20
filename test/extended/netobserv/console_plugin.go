84e657002c9d2a36aa555224b0dbc50aab917b65
Kasturi Narra <knarra@redhat.com>
2022-06-20 16:10:59 +0530

    Removing test folder

--- test/extended/netobserv/console_plugin.go
+++
@@ -1,8 +0,0 @@
-package netobserv
-
-type consoleplugin struct {
-	Image string `json:"image"`
-	Port  int    `json:"port"`
-}
-
-// place holder for console plugin functions
--- test/extended/testdata/logging/external-log-stores/kafka/zookeeper-configmap/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-[ -d /var/lib/zookeeper/data ] || mkdir /var/lib/zookeeper/data
-[ -z "$ID_OFFSET" ] && ID_OFFSET=1
-export ZOOKEEPER_SERVER_ID=$((${HOSTNAME##*-} + $ID_OFFSET))
-echo "${ZOOKEEPER_SERVER_ID:-1}" | tee /var/lib/zookeeper/data/myid
-cp -Lur /etc/kafka-configmap/* /etc/kafka/
